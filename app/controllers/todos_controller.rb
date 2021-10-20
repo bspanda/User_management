68d71e13deb0295c5a508dc3453573ede884c03d
BSPANDA <panda.bs@raghuenggcollege.in>
2021-10-20 14:40:15 +0530

    modifications by bspanda

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,13 +3,15 @@ class TodosController < ApplicationController
 
   def index
     # render plain: "Hello, this is /todos! #{DateTime.now.to_s(:short)}"
-    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    #render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    render "index"
   end
 
   def show
     id = params[:id]
     todo = Todo.find(id)
-    render plain: todo.to_pleasant_string
+    #render plain: todo.to_pleasant_string
+    render "todo"
   end
 
   def create
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,20 @@
+require "date"
+require "active_record"
+
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[]"
-    "#{id}.  #{due_date.to_s(:short)}  #{todo_text} #{is_completed}"
+  def self.overdue
+    all.where("completed = ? and due_date < ? ", false, Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
   end
 end
